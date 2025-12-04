fdb8431b7ecaf8dc35ccf9234dcd5b08e9a1da93
Kantemir <kan.shandir94@gmail.com>
2025-12-04 23:43:51 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StartPage from './StartPage';
-import RecordsPage from './RecordsPage';
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<StartPage />} />
-          <Route path="/records" element={<RecordsPage />} />
         </Routes>
       </div>
     </Router>
