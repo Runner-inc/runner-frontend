63d92d8e328a655a5fb69a4bcefdd5f638932e22
Kantemir <kan.shandir94@gmail.com>
2025-12-10 16:05:05 +0300

    fix

--- src/StartPage.jsx
+++ src/StartPage.jsx
@@ -181,10 +181,10 @@ function StartPage() {
       const sBottom = sTop + collisionSize;
 
       const collision = (
-        vLeft > sRight &&    // Viking left < Skeleton right
-        vRight < sLeft &&    // Viking right > Skeleton left
-        vTop < sBottom &&    // Viking top < Skeleton bottom
-        vBottom > sTop       // Viking bottom > Skeleton top
+        vLeft <= sRight &&    // Viking left < Skeleton right
+        vRight <= sLeft &&    // Viking right > Skeleton left
+        vTop <= sBottom &&    // Viking top < Skeleton bottom
+        vBottom >= sTop       // Viking bottom > Skeleton top
       );
       // const collision = (
       //   vRight < sLeft &&
